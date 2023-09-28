4bcb10a6b7d0e94b093beeca377b8e047e679474
Festus <festusgitahik@gmail.com>
2023-09-28 18:19:45 +0300

    changed db structue

--- database/ambulance.sql
+++ database/ambulance.sql
@@ -1,36 +1,41 @@
 CREATE TABLE ratings (
-    rating_id INT AUTO_INCREMENT PRIMARY KEY,
+    rating_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     rating_value INT NOT NULL,
     comments TEXT,
-    request_id INT
+    request_id BIGINT
 );
 CREATE TABLE ambulances (
-    ambulance_id INT AUTO_INCREMENT PRIMARY KEY,
+    ambulance_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     ambulance_plate VARCHAR(20) UNIQUE,
     in_use BOOLEAN DEFAULT FALSE,
     issuing_organization varchar(255),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME,
     in_contract BOOLEAN DEFAULT TRUE,
-    current_request_id INT -- FOREIGN KEY (current_request_id) REFERENCES ambulance_requests(request_id)
+    current_request_id BIGINT -- FOREIGN KEY (current_request_id) REFERENCES ambulance_requests(request_id)
 );
 CREATE TABLE ambulance_requests (
-    request_id INT AUTO_INCREMENT PRIMARY KEY,
-    patient_name VARCHAR(255) NOT NULL,
+    request_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    patient_id BIGINT unsigned NOT NULL,
     pickup_location VARCHAR(255) NOT NULL,
+    destination_address VARCHAR(255) NOT NULL,
+    distance VARCHAR(50) NOT NULL,
+    pickup_date DATETIME NOT NULL,
+    patient_state TEXT NOT NULL,
     request_datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status ENUM(
+    request_status ENUM(
         'Pending',
         'Dispatched',
         'Arrived',
         'Completed',
         'Canceled'
     ) NOT NULL,
-    emergency_nature VARCHAR(50),
-    emergency_severity VARCHAR(50),
-    ambulance_id INT,
+    -- emergency_nature VARCHAR(50),
+    -- emergency_severity VARCHAR(50),
+    ambulance_id BIGINT,
+    rating_id BIGINT,
+    FOREIGN KEY (patient_id) REFERENCES users(id),
     FOREIGN KEY (ambulance_id) REFERENCES ambulances(ambulance_id),
-    rating_id INT,
     FOREIGN KEY (rating_id) REFERENCES ratings(rating_id)
 );
 ALTER TABLE ambulances
